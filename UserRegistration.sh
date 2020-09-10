f9e42055ceffd4576c99ed57a136adb252c9c0d5
unknown <log2aayusharyan@gmail.com>
2020-09-10 13:25:40 -0700

    writing pattern for Validating first Name :

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -x
 
 echo "--------Welcome to User Registration form:-------"
+shopt -s
+
+function firstName () {
+	echo "Enter the first Name :"
+	read FirstName
+   firstNamePattern="^[A-Z]{1}[a-z]{2,}$"
+
+	if [[ $FirstName =~ $firstNamePattern ]]
+	then
+		echo "enter name is Valid"
+	else
+		echo "enter name is Invalid : PLZ starts with Caps letters;"
+	fi
+}
+firstName
